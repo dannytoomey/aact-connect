5626c7fe427280b8f0c8ffa7019d170909f509d9
dannytoomey <dtoomey@umail.ucsb.edu>
2023-04-10 19:32:54 -0700

    update 4.10.23

--- README.md
+++ README.md
@@ -12,8 +12,6 @@ This repository contains a command line executable python file (aact-connect.py)
 
 4. Install dependencies with `python3 -m pip install -r requirements.txt`
 
-5. Activate the virtual environment with `source env/bin/activate`
-
 ## Usage
 
 There are several flags you can use to run different commands
@@ -27,16 +25,19 @@ _Optional: To avoid having to re-enter your credentials every time you run the c
 [password]
 ```
 
-The results of the search will be in the `queries` directory with the file name `query_[number of results]_results.csv`
+The results of the search will be in the `queries` directory with the file name `query_[number of results]_results_[search date].csv`
 
 To add additional data of interest to the search results, enter
 `python aact-connect.py -u [your acct username] -p [your aact password] -s -a`
+
+And to add additional data on a search that was already performed, enter
+`python aact-connect.py -u [your acct username] -p [your aact password] -a -us [path of CSV file with prior search results]`
 
 The results of the data additions will be in the `additional_data` directory with the file name `query_[number of results]_additional_data.csv`
 
 _Note: This operation can take a few minutes. A progress bar with an estimate time to completion is provided_
 
-To search for adverse event data for a given clinical trials, enter
+To search for adverse event data for a given clinical trial, enter
 `python aact-connect.py -u [your acct username] -p [your aact password] -l [NCT id]`
 
-The results of the AE look up will be in the `AE_lookup` directory with the file name `AE_lookup_[NCT id].csv`
+The results of the AE look up will be in the `AE_lookup` directory with the file name `AE_lookup_[NCT id]_[search date].csv`
--- aact-connect.py
+++ aact-connect.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pandas.io.sql as sqlio
 from functools import reduce
 import progressbar, time
+from datetime import datetime
 
 cwd = os.getcwd()
 
@@ -13,6 +14,7 @@ if os.path.exists(cwd+"/private/myconfig.txt"):
   parser = argparse.ArgumentParser()
   parser.add_argument("-s", "--search", action="store_true", required=False)
   parser.add_argument("-a", "--add", action="store_true", required=False)
+  parser.add_argument("-us", "--use_search", required=False)
   parser.add_argument("-l", "--lookup", required=False)
   parser.add_argument("-ae", "--AE_num", required=False)
   
@@ -32,6 +34,7 @@ else:
   parser.add_argument("-p", "--password", required=True)
   parser.add_argument("-s", "--search", action="store_true", required=False)
   parser.add_argument("-a", "--add", action="store_true", required=False)
+  parser.add_argument("-us", "--use_search", required=False)
   parser.add_argument("-l", "--lookup", required=False)
   parser.add_argument("-ae", "--AE_num", required=False)
   
@@ -51,89 +54,96 @@ if args.search:
   print("Performing query...")
 
   cursor = aact_connect.cursor()
-
   
   query = '''
 
-  select * from studies where    
-  study_type = 'Interventional'   
-  and   
-  enrollment < 1000   
-  and   
-  (   
-  (official_title ilike '%challenge%') or   
-  (official_title ilike '%immunization%' and official_title ilike '%sporozoites%') or   
-  (official_title ilike '%human%' and official_title ilike '%carriage%') or   
-  (official_title ilike '%infection%' and   
-  (official_title ilike '%controlled%' or official_title ilike '%experimental%' or official_title ilike '%induced%')) or    
-  (official_title ilike '%efficacy%' and official_title ilike '%vaccine%') or   
-  (official_title ilike '%human%' and official_title ilike '%exposure%') or   
-  (official_title ilike '%healthy%' and   
-  (official_title ilike '%naïve%' or official_title ilike '%naive%')) or    
-  (official_title ilike '%competitive%' and official_title ilike '%carriage%')    
-  OR    
-  (brief_title ilike '%challenge%') or    
-  (brief_title ilike '%immunization%' and brief_title ilike '%sporozoites%') or   
-  (brief_title ilike '%human%' and brief_title ilike '%carriage%') or   
-  (brief_title ilike '%infection%' and    
-  (brief_title ilike '%controlled%' or brief_title ilike '%experimental%' or brief_title ilike '%induced%')) or   
-  (brief_title ilike '%efficacy%' and brief_title ilike '%vaccine%') or   
-  (brief_title ilike '%human%' and brief_title ilike '%exposure%') or   
-  (brief_title ilike '%healthy%' and    
-  (brief_title ilike '%naïve%' or brief_title ilike '%naive%')) or    
-  (brief_title ilike '%competitive%' and brief_title ilike '%carriage%')    
-  OR    
-  (acronym ilike '%challenge%') or    
-  (acronym ilike '%human%')   
-  OR    
-  nct_id IN   
-  (select s.nct_id from studies s, keywords k where   
-  s.nct_id = k.nct_id and k.name ilike '%challenge%')   
-  OR    
-  nct_id IN   
-  (select s.nct_id from studies s, detailed_descriptions d where    
-  s.nct_id = d.nct_id and   
-  ((d.description ilike '%challenge%') and    
-  (d.description ilike '%infection%' or   
-  d.description ilike '%controlled%' or   
-  d.description ilike '%experimental%')))   
-  OR    
-  nct_id IN   
-  (select s.nct_id from studies s, brief_summaries b where    
-  s.nct_id = b.nct_id and   
-  ((b.description ilike '%challenge%') and    
-  (b.description ilike '%infection%' or   
-  b.description ilike '%controlled%' or   
-  b.description ilike '%experimental%')))   
-  )   
+  select * from studies where
+  study_type = 'Interventional'
+  and
+  enrollment < 1000
+  and
+  study_first_submitted_date < '2022-06-30'
+  and
+  (
+  (official_title ilike '%challenge%') or
+  (official_title ilike '%immunization%' and official_title ilike '%sporozoites%') or
+  (official_title ilike '%human%' and official_title ilike '%carriage%') or
+  (official_title ilike '%infection%' and
+  (official_title ilike '%controlled%' or official_title ilike '%experimental%' or official_title ilike '%induced%')) or
+  (official_title ilike '%efficacy%' and official_title ilike '%vaccine%') or
+  (official_title ilike '%human%' and official_title ilike '%exposure%') or
+  (official_title ilike '%healthy%' and
+  (official_title ilike '%naïve%' or official_title ilike '%naive%')) or
+  (official_title ilike '%competitive%' and official_title ilike '%carriage%')
+  OR
+  (brief_title ilike '%challenge%') or
+  (brief_title ilike '%immunization%' and brief_title ilike '%sporozoites%') or
+  (brief_title ilike '%human%' and brief_title ilike '%carriage%') or
+  (brief_title ilike '%infection%' and
+  (brief_title ilike '%controlled%' or brief_title ilike '%experimental%' or brief_title ilike '%induced%')) or
+  (brief_title ilike '%efficacy%' and brief_title ilike '%vaccine%') or
+  (brief_title ilike '%human%' and brief_title ilike '%exposure%') or
+  (brief_title ilike '%healthy%' and
+  (brief_title ilike '%naïve%' or brief_title ilike '%naive%')) or
+  (brief_title ilike '%competitive%' and brief_title ilike '%carriage%')
+  OR
+  (acronym ilike '%challenge%') or
+  (acronym ilike '%human%')
+  OR
+  nct_id IN
+  (select s.nct_id from studies s, keywords k where
+  s.nct_id = k.nct_id and k.name ilike '%challenge%')
+  OR
+  nct_id IN
+  (select s.nct_id from studies s, detailed_descriptions d where
+  s.nct_id = d.nct_id and
+  ((d.description ilike '%challenge%') and
+  (d.description ilike '%infection%' or
+  d.description ilike '%controlled%' or
+  d.description ilike '%experimental%')))
+  OR
+  nct_id IN
+  (select s.nct_id from studies s, brief_summaries b where
+  s.nct_id = b.nct_id and
+  ((b.description ilike '%challenge%') and
+  (b.description ilike '%infection%' or
+  b.description ilike '%controlled%' or
+  b.description ilike '%experimental%')))
+  ) 
 
   
   '''
   
-
 
   data = sqlio.read_sql_query(query,aact_connect)
   query_frame = pd.DataFrame(data)
   query_frame.to_csv(cwd+"/queries/query_results.csv",header=True,index=True)
 
   read_csv = pd.read_csv(cwd+"/queries/query_results.csv")
-  count = len(read_csv) 
+  count = len(read_csv)-1
   print("Query returned "+str(count)+" results")
 
-  if os.path.exists(cwd+"/queries/query_"+str(count)+"_results.csv"):
-    os.remove(cwd+"/queries/query_"+str(count)+"_results.csv")
-    os.rename(cwd+"/queries/query_results.csv",cwd+"/queries/query_"+str(count)+"_results.csv")
+  if os.path.exists(cwd+"/queries/query_"+str(count)+"_results_"+datetime.today().strftime('%Y-%m-%d')+".csv"):
+    os.remove(cwd+"/queries/query_"+str(count)+"_results_"+datetime.today().strftime('%Y-%m-%d')+".csv")
+    os.rename(cwd+"/queries/query_results.csv",cwd+"/queries/query_"+str(count)+"_results_"+datetime.today().strftime('%Y-%m-%d')+".csv")
   else:
-    os.rename(cwd+"/queries/query_results.csv",cwd+"/queries/query_"+str(count)+"_results.csv")
+    os.rename(cwd+"/queries/query_results.csv",cwd+"/queries/query_"+str(count)+"_results_"+datetime.today().strftime('%Y-%m-%d')+".csv")
 
+  last_search = cwd+"/queries/query_"+str(count)+"_results_"+datetime.today().strftime('%Y-%m-%d')+".csv"
 
+  if(args.add == None and args.lookup == None and args.AE_num == None):
+    aact_connect.close()
+  
 if args.add:
 
   print("Adding additional results...")
 
-  query_frame = pd.read_csv("AACT_6_5085_results.csv")
+  if(args.use_search != None):
+    query_frame = pd.read_csv(cwd+"/"+args.use_search)
+  else:
+    query_frame = pd.read_csv(last_search)
 
-  nct_id_array = query_frame['nct_id'].to_list() # ['NCT01895855','NCT04150250'] 
+  nct_id_array = query_frame['nct_id'].to_list() 
   
   widgets = [' [',progressbar.Timer(format= 'elapsed time: %(elapsed)s'),'] ',progressbar.Bar('*'),' (',progressbar.ETA(), ') ',]
   bar = progressbar.ProgressBar(max_value=200,widgets=widgets).start()
@@ -142,99 +152,72 @@ if args.add:
     
     trial = pd.DataFrame(query_frame.loc[query_frame['nct_id'] == nct_id])
 
-    query_add_1 = "select number_of_nsae_subjects from calculated_values where nct_id = '"+nct_id+"'"
-    query_add_2 = "select minimum_age_num from calculated_values where nct_id = '"+nct_id+"'"
-    query_add_3 = "select maximum_age_num from calculated_values where nct_id = '"+nct_id+"'"
-    query_add_4 = "select recruitment_details from participant_flows where nct_id = '"+nct_id+"'"
-    query_add_5 = "select ci_percent, p_value from outcome_analyses where nct_id = '"+nct_id+"'"
-    query_add_6 = "select description from design_groups where nct_id = '"+nct_id+"'"
-    query_add_7 = "select description from interventions where nct_id = '"+nct_id+"'"
-    query_add_8 = "select pmid, citation from study_references where nct_id = '"+nct_id+"'"
-    query_add_9 = "select SUM(subjects_affected) AS AE_Count from reported_event_totals where nct_id = '"+nct_id+"' AND classification = 'Total, other adverse events'"
-    query_add_10 = "select SUM(subjects_affected) AS SAE_Count from reported_event_totals where nct_id = '"+nct_id+"' AND classification = 'Total, serious adverse events'"
-    query_add_11 = "select SUM(subjects_affected) AS Mortality_Count from reported_event_totals where nct_id = '"+nct_id+"' AND classification = 'Total, all-cause mortality'"
-    query_add_12 = "select COUNT(DISTINCT adverse_event_term) AS Num_AEs_described from reported_events where nct_id = '"+nct_id+"'"
-  
+    combined_query_1 = '''\
+    select calculated_values.nct_id, calculated_values.number_of_nsae_subjects, calculated_values.minimum_age_num, calculated_values.maximum_age_num,
+           participant_flows.recruitment_details,
+           outcome_analyses.ci_percent,outcome_analyses.p_value,
+           design_groups.description,
+           interventions.description,
+           study_references.pmid,study_references.citation,
+           COUNT(DISTINCT reported_events.adverse_event_term) AS Num_AEs_described 
+
+    from calculated_values
+    left join participant_flows
+      on participant_flows.nct_id = calculated_values.nct_id 
+    left join outcome_analyses
+      on outcome_analyses.nct_id = calculated_values.nct_id 
+    left join design_groups
+      on design_groups.nct_id = calculated_values.nct_id 
+    left join interventions
+      on interventions.nct_id = calculated_values.nct_id 
+    left join study_references
+      on study_references.nct_id = calculated_values.nct_id
+    left join reported_events
+      on reported_events.nct_id = calculated_values.nct_id
+
+    where 
+      calculated_values.nct_id = '{}' 
+
+    group by
+      calculated_values.nct_id, calculated_values.number_of_nsae_subjects, calculated_values.minimum_age_num, calculated_values.maximum_age_num,
+      participant_flows.recruitment_details,
+      outcome_analyses.ci_percent,outcome_analyses.p_value,
+      design_groups.description,
+      interventions.description,
+      study_references.pmid,study_references.citation
     
-    data_add_1 = sqlio.read_sql_query(query_add_1,aact_connect)
-    query_frame_add_1 = pd.DataFrame(data_add_1)
-    query_frame_add_1['nct_id'] = nct_id
-    query_frame_add_1 = query_frame_add_1.iloc[:1]
-    data_concat = trial.merge(query_frame_add_1,on="nct_id",how="left")
+    fetch first 1 rows only
+          
+    \
+    '''.format(nct_id)
+
+    combined_query_2 = '''\
+    select reported_event_totals.nct_id,
+           sum(case when reported_event_totals.classification = 'Total, other adverse events' then 
+              reported_event_totals.subjects_affected else 0 end) as AE_Count,
+           sum(case when reported_event_totals.classification = 'Total, serious adverse events' then 
+              reported_event_totals.subjects_affected else 0 end) as SAE_Count,
+           sum(case when reported_event_totals.classification = 'Total, all-cause mortality' then 
+              reported_event_totals.subjects_affected else 0 end) as Mortality_Count
+    from reported_event_totals
+
+    where 
+      reported_event_totals.nct_id = '{}' 
     
-    data_add_2 = sqlio.read_sql_query(query_add_2,aact_connect)
-    query_frame_add_2 = pd.DataFrame(data_add_2)
-    query_frame_add_2['nct_id'] = nct_id
-    query_frame_add_2 = query_frame_add_2.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_2,on="nct_id",how="left")
-    
-    data_add_3 = sqlio.read_sql_query(query_add_3,aact_connect)
-    query_frame_add_3 = pd.DataFrame(data_add_3)
-    query_frame_add_3['nct_id'] = nct_id
-    query_frame_add_3 = query_frame_add_3.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_3,on="nct_id",how="left")
-    
-    query_add_4 = "select recruitment_details from participant_flows where nct_id = '"+nct_id+"'"
-    data_add_4 = sqlio.read_sql_query(query_add_4,aact_connect)
-    query_frame_add_4 = pd.DataFrame(data_add_4)
-    query_frame_add_4['nct_id'] = nct_id
-    query_frame_add_4 = query_frame_add_4.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_4,on="nct_id",how="left")
-   
-    query_add_5 = "select ci_percent, p_value from outcome_analyses where nct_id = '"+nct_id+"'"
-    data_add_5 = sqlio.read_sql_query(query_add_5,aact_connect)
-    query_frame_add_5 = pd.DataFrame(data_add_5)
-    query_frame_add_5 = query_frame_add_5.iloc[:1]
-    query_frame_add_5['nct_id'] = nct_id
-    data_concat = data_concat.merge(query_frame_add_5,on="nct_id",how="left")
+    group by
+      reported_event_totals.nct_id
 
-    data_add_6 = sqlio.read_sql_query(query_add_6,aact_connect)
-    query_frame_add_6 = pd.DataFrame(data_add_6)
-    query_frame_add_6['nct_id'] = nct_id
-    query_frame_add_6 = query_frame_add_6.iloc[:1]
-    query_frame_add_6 = query_frame_add_6.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_6,on="nct_id",how="left")
+    \
+    '''.format(nct_id)
 
-    data_add_7 = sqlio.read_sql_query(query_add_7,aact_connect)
-    query_frame_add_7 = pd.DataFrame(data_add_7)
-    query_frame_add_7['nct_id'] = nct_id
-    query_frame_add_7 = query_frame_add_7.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_7,on="nct_id",how="left")
 
-    data_add_8 = sqlio.read_sql_query(query_add_8,aact_connect)
-    query_frame_add_8 = pd.DataFrame(data_add_8)
-    query_frame_add_8['nct_id'] = nct_id
-    query_frame_add_8 = query_frame_add_8.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_8,on="nct_id",how="left")
-
-    data_add_9 = sqlio.read_sql_query(query_add_9,aact_connect)
-    query_frame_add_9 = pd.DataFrame(data_add_9)
-    query_frame_add_9['nct_id'] = nct_id
-    query_frame_add_9 = query_frame_add_9.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_9,on="nct_id",how="left")
-
-    data_add_10 = sqlio.read_sql_query(query_add_10,aact_connect)
-    query_frame_add_10 = pd.DataFrame(data_add_10)
-    query_frame_add_10['nct_id'] = nct_id
-    query_frame_add_10 = query_frame_add_10.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_10,on="nct_id",how="left")
-
-    data_add_11 = sqlio.read_sql_query(query_add_11,aact_connect)
-    query_frame_add_11 = pd.DataFrame(data_add_11)
-    query_frame_add_11['nct_id'] = nct_id
-    query_frame_add_11 = query_frame_add_11.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_11,on="nct_id",how="left")
-
-    data_add_12 = sqlio.read_sql_query(query_add_12,aact_connect)
-    query_frame_add_12 = pd.DataFrame(data_add_12)
-    query_frame_add_12['nct_id'] = nct_id
-    query_frame_add_12 = query_frame_add_12.iloc[:1]
-    data_concat = data_concat.merge(query_frame_add_12,on="nct_id",how="left")
-    
+    data_add_1 = sqlio.read_sql_query(combined_query_1,aact_connect)
+    data_concat = trial.merge(data_add_1,on="nct_id",how="left")
+    data_add_2 = sqlio.read_sql_query(combined_query_2,aact_connect)
+    data_concat = data_concat.merge(data_add_2,on="nct_id",how="left")
 
     if nct_id == nct_id_array[0]:
       result_concat = data_concat
-      
     else:
       result_concat = data_concat.append(result_concat,ignore_index=True)
       
@@ -243,14 +226,16 @@ if args.add:
   result_concat.to_csv(cwd+"/additional_data/query_additional_data.csv",header=True,index=False)
 
   read_csv = pd.read_csv(cwd+"/additional_data/query_additional_data.csv")
-  count = len(read_csv) 
-  print("Added additional data for "+str(count)+" results")
+  count = len(read_csv)-1 
+  print("\nAdded additional data for "+str(count)+" results")
 
-  if os.path.exists(cwd+"/additional_data/query_"+str(count)+"_additional_data.csv"):
-    os.remove(cwd+"/additional_data/query_"+str(count)+"_additional_data.csv")
-    os.rename(cwd+"/additional_data/query_additional_data.csv",cwd+"/additional_data/query_"+str(count)+"_additional_data.csv")
+  if os.path.exists(cwd+"/additional_data/query_"+str(count)+"_additional_data_"+datetime.today().strftime('%Y-%m-%d')+".csv"):
+    os.remove(cwd+"/additional_data/query_"+str(count)+"_additional_data_"+datetime.today().strftime('%Y-%m-%d')+".csv")
+    os.rename(cwd+"/additional_data/query_additional_data.csv",cwd+"/additional_data/query_"+str(count)+"_additional_data_"+datetime.today().strftime('%Y-%m-%d')+".csv")
   else:
-    os.rename(cwd+"/additional_data/query_additional_data.csv",cwd+"/additional_data/query_"+str(count)+"_additional_data.csv")
+    os.rename(cwd+"/additional_data/query_additional_data.csv",cwd+"/additional_data/query_"+str(count)+"_additional_data_"+datetime.today().strftime('%Y-%m-%d')+".csv")
+  
+  aact_connect.close()
 
 if args.lookup != None:
 
@@ -271,7 +256,9 @@ if args.lookup != None:
     else:
       data_concat = data_concat.append(query_frame,ignore_index=True)
   
-  data_concat.to_csv(cwd+"/AE_lookup/AE_lookup_"+args.lookup+".csv",header=True,index=False)
+  data_concat.to_csv(cwd+"/AE_lookup/AE_lookup_"+args.lookup+"_"+datetime.today().strftime('%Y-%m-%d')+".csv",header=True,index=False)    
+
+  aact_connect.close()
 
 if args.AE_num != None:
 
@@ -284,6 +271,7 @@ if args.AE_num != None:
   query_frame['nct_id'] = str(args.AE_num)
   data_concat = query_frame
 
-  data_concat.to_csv(cwd+"/AE_lookup/AE_count_"+args.AE_num+".csv",header=True,index=False) 
+  data_concat.to_csv(cwd+"/AE_lookup/AE_count_"+args.AE_num+"_"+datetime.today().strftime('%Y-%m-%d')+".csv",header=True,index=False) 
 
+  aact_connect.close()
 
